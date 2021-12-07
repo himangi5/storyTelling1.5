c0296defa523d1937832031cff541ef50af6208b
himangi5 <79082119+himangi5@users.noreply.github.com>
2021-12-07 15:05:07 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyB09onouge6oh0j6m2iOPzjUjrIjcCh5TU",
+    authDomain: "storytelling-1d1c1.firebaseapp.com",
+    databaseURL: "https://storytelling-1d1c1-default-rtdb.firebaseio.com",
+    projectId: "storytelling-1d1c1",
+    storageBucket: "storytelling-1d1c1.appspot.com",
+    messagingSenderId: "584936575789",
+    appId: "1:584936575789:web:2ba834293d7212f05cf9a4"
+};
